da51b8f0717ef2844ef566b017ff2cd1a442dfe1
Lucas Morales <lmorales@edx.org>
2014-11-16 02:22:37 -0500

    walls

--- front/game.js
+++ front/game.js
@@ -1,27 +1,19 @@
-SIZE = 5;
-walls = generateMap(SIZE, .3);
+SIZE = 50;
+walls = generateMap(SIZE, .1, .03, 2);
 Game = {
 	// This defines our grid's size and the size of each of its tiles
-	map_grid: {
-		width:	SIZE,
-		height: SIZE,
-		tile: {
-			width:	16,
-			height: 16
-		},
-		border: 8
-	},
+	tile: 16,
  
 	// The total width of the game screen. Since our grid takes up the entire screen
 	//	this is just the width of a tile times the width of the grid
 	width: function() {
-		return this.map_grid.width * (this.map_grid.tile.width + this.map_grid.border) + this.map_grid.border;
+		return SIZE * this.tile;
 	},
  
 	// The total height of the game screen. Since our grid takes up the entire screen
 	//	this is just the height of a tile times the height of the grid
 	height: function() {
-		return this.map_grid.height * (this.map_grid.tile.height + this.map_grid.border) + this.map_grid.border;
+		return SIZE * this.tile;
 	},
  
 	// Initialize and start our game
@@ -31,34 +23,69 @@ Game = {
 		Crafty.background('rgb(0, 128, 255)');
  
 		// Place a tree at every edge square on our grid of 16x16 tiles
-		[[0,0],[0,Game.height()-Game.map_grid.border],[0,0],[Game.width()-Game.map_grid.border,0]]
-			.forEach(function(coord, i){
-				Crafty.e('2D, Canvas, Color')
-					.attr({
-					  x: coord[0],
-					  y: coord[1],
-					  w: i<2?Game.width():Game.map_grid.border,
-					  h: i<2?Game.map_grid.border:Game.height()
-					})
-					.color('rgb(20, 185, 40)');
-			});
-		for (var i=0; i<SIZE; i++) {
-			for (var j=0; j<SIZE; j++) {
-				['top','bottom','left','right'].forEach(function(k, q){
-					var m = (walls[i][j][k] && walls.match(i,j,k));
-					if (m) {
-						console.log('match',i,j,k,walls);
-						Crafty.e('2D, Canvas, Color')
-							.attr({
-								x: i?i*(Game.map_grid.tile.width * (1 + (q==3)) + Game.map_grid.border):(q==3)*Game.map_grid.tile.width,
-								y: j?j*(Game.map_grid.tile.height * (1 + (q==1)) + Game.map_grid.border):(q==1)*Game.map_grid.tile.height,
-								w: q<2?Game.map_grid.tile.width+Game.map_grid.border/2:Game.map_grid.border,
-								h: q<2?Game.map_grid.border:Game.map_grid.tile.height+Game.map_grid.border/2
-							})
-							.color('rgb(20,185,40)');
-					}
-				});
-			}
+		for (var i=0; i<SIZE; i++)
+			for (var j=0; j<SIZE; j++)
+				if (walls[i][j]===null)
+					Crafty.e('Block')
+						.at(i,j)
+						.color('rgb(20, 185, 40)');
+		var s;
+		while(true){
+			s = [Math.floor(Math.random()*SIZE), Math.floor(Math.random()*SIZE)];
+			if(walls[s[0]]&&(walls[s[0]][s[1]]!==null))break
 		}
+		Crafty.e('PlayerCharacter').at(s[0],s[1]);
 	}
-}
+};
+
+Crafty.c('Grid', {
+  init: function() {
+    this.attr({
+      w: Game.tile,
+      h: Game.tile
+    })
+  },
+ 
+  // Locate this entity at the given position on the grid
+  at: function(x, y) {
+    if (x === undefined && y === undefined) {
+      return { x: this.x/Game.tile, y: this.y/Game.tile }
+    } else {
+      this.attr({ x: x * Game.tile, y: y * Game.tile });
+    }
+	return this;
+  }
+});
+
+Crafty.c('Actor', {
+	init: function() {
+		this.requires('2D, Canvas, Grid');
+	},
+});
+
+Crafty.c('Block', {
+  init: function() {
+    this.requires('Actor, Solid, Color');
+  },
+});
+
+Crafty.c('PlayerCharacter', {
+  init: function() {
+    this.requires('Actor, Fourway, Color, Collision')
+      .fourway(4)
+	  .stopOnSolids()
+      .color('rgb(0, 0, 0)')
+	  .collision(new Crafty.circle(8,8,6));
+  },
+  stopOnSolids: function() {
+     this.onHit('Solid', this.stopMovement);
+     return this;
+   },
+   stopMovement: function() {
+   		this._speed = 0;
+   		if (this._movement) {
+   			this.x -= this._movement.x;
+   			this.y -= this._movement.y;
+   		}
+   	},
+});
--- mappy.js
+++ mappy.js
@@ -1,4 +1,5 @@
 
+console.log(generateMap(20,0.1,0.05, 2));
 //3rd arg = how likey it is to have a 1 when neighbor is a 1
 //4th arg = how many times to add new borders
 function generateMap(SIZE, BORDER_CHANCE, NEIGHBOR_BORDER_CHANCE, NUM_PASSES){
@@ -11,9 +12,27 @@ function generateMap(SIZE, BORDER_CHANCE, NEIGHBOR_BORDER_CHANCE, NUM_PASSES){
 
 		for(var col = 0; col < SIZE; col++){
 			//make block, set borders on outside edges
-			map[row][col] = Math.random()<BORDER_CHANCE?1:0		
+			map[row][col] = -1;	
 		}
 	}
+
+
+
+
+	//chunk time
+	var chunks = getChunkiness();
+	for(var ch = 1; ch < chunks+1; ch++){
+		var found = false;
+		while(!found){
+			var rr = Math.floor(Math.random()*SIZE);
+			var cc = Math.floor(Math.random()*SIZE);
+			if(map[rr][cc] == -1){
+				map[rr][cc] = ch;
+				found = true;
+			}
+		}
+	}
+
 
 	var OFFSETS = {
 		'top':{row:-1,col:0},
@@ -21,6 +40,50 @@ function generateMap(SIZE, BORDER_CHANCE, NEIGHBOR_BORDER_CHANCE, NUM_PASSES){
 		'bottom':{row:1,col:0},
 		'right':{row:0,col:1}
 	};
+
+
+
+	while(needsChunking(map)){
+
+		var cmap = new Array(SIZE);
+		
+		//make blocks
+		for(var row = 0; row < SIZE; row++){
+			cmap[row] = new Array(SIZE);
+			for(var col = 0; col < SIZE; col++){
+				//make block, set borders on outside edges
+				cmap[row][col] = -1;	
+			}
+		}
+
+		for(var row = 0; row < SIZE; row++){
+			for(var col = 0; col < SIZE; col++){
+				if(map[row][col] != -1){
+					cmap[row][col] = map[row][col];
+					var keys = Object.keys(OFFSETS);
+					for(var k = 0; k < 4; k++){
+						var re = row + OFFSETS[keys[k]].row;
+						var ce = col + OFFSETS[keys[k]].col;
+						if(map[re] && map[re][ce] && map[re][ce] == -1){
+							cmap[re][ce] = map[row][col];
+						}
+					}
+				}	
+			}
+		}
+
+		map = cmap;
+	}
+
+
+
+
+	for(var row = 0; row < SIZE; row++){
+		for(var col = 0; col < SIZE; col++){
+			//make block, set borders on outside edges
+			map[row][col] = Math.random()<BORDER_CHANCE?0:map[row][col];		
+		}
+	}
 
 	for(var pass = 0; pass < NUM_PASSES; pass++){
 		for(var row = 0; row < SIZE; row++){
@@ -35,12 +98,32 @@ function generateMap(SIZE, BORDER_CHANCE, NEIGHBOR_BORDER_CHANCE, NUM_PASSES){
 					}
 				}
 				if(Math.random() < p){
-					map[row][col] = 1;
+					map[row][col] = 0;
 				}
 
 			}
 		}
 	}
 
+
+
 	return map;
+}
+
+function needsChunking(m){
+	for(var r = 0; r < m.length; r++){
+		for(var c = 0; c < m[r].length; c++){
+			if(m[r][c] == -1){
+
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+
+function getChunkiness(){
+	return 10;
 }
