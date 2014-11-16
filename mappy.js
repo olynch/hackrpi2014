8231456ade76f10cff663398aacd5bfb887562a4
Akshay Thejaswi <chickenbellyfinn@gmail.com>
2014-11-16 02:25:18 -0500

    neighbor chance and passes args

--- mappy.js
+++ mappy.js
@@ -1,5 +1,7 @@
 
-function generateMap(SIZE, BORDER_CHANCE){
+//3rd arg = how likey it is to have a 1 when neighbor is a 1
+//4th arg = how many times to add new borders
+function generateMap(SIZE, BORDER_CHANCE, NEIGHBOR_BORDER_CHANCE, NUM_PASSES){
 
 	var map = new Array(SIZE);
 	
@@ -20,21 +22,23 @@ function generateMap(SIZE, BORDER_CHANCE){
 		'right':{row:0,col:1}
 	};
 
-	for(var row = 0; row < SIZE; row++){
+	for(var pass = 0; pass < NUM_PASSES; pass++){
+		for(var row = 0; row < SIZE; row++){
 
-		for(var col = 0; col < SIZE; col++){
-			var p = 0;
-			for(var key in OFFSETS){
-				var nr = row + OFFSETS[key].row;
-				var nc = row + OFFSETS[key].col;
-				if(nr >= 0 && nr < SIZE && nc >= 0 && nc < SIZE){
-					p += 0.05;
+			for(var col = 0; col < SIZE; col++){
+				var p = 0;
+				for(var key in OFFSETS){
+					var nr = row + OFFSETS[key].row;
+					var nc = row + OFFSETS[key].col;
+					if(nr >= 0 && nr < SIZE && nc >= 0 && nc < SIZE){
+						p += NEIGHBOR_BORDER_CHANCE;
+					}
 				}
-			}
-			if(Math.random() < p){
-				map[row][col] = 1;
-			}
+				if(Math.random() < p){
+					map[row][col] = 1;
+				}
 
+			}
 		}
 	}
 
