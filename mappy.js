5399f902ee0c7e5434893bce80a5b554a96b4778
Akshay Thejaswi <chickenbellyfinn@gmail.com>
2014-11-16 02:20:28 -0500

    new map format

--- mappy.js
+++ mappy.js
@@ -1,3 +1,4 @@
+
 function generateMap(SIZE, BORDER_CHANCE){
 
 	var map = new Array(SIZE);
@@ -8,12 +9,7 @@ function generateMap(SIZE, BORDER_CHANCE){
 
 		for(var col = 0; col < SIZE; col++){
 			//make block, set borders on outside edges
-			map[row][col] = {
-				'top':row == 0,
-				'left':col == 0,
-				'bottom':row == SIZE-1,
-				'right':col == SIZE-1				
-			};			
+			map[row][col] = Math.random()<BORDER_CHANCE?1:0		
 		}
 	}
 
@@ -24,42 +20,23 @@ function generateMap(SIZE, BORDER_CHANCE){
 		'right':{row:0,col:1}
 	};
 
-	//generate borders
 	for(var row = 0; row < SIZE; row++){
+
 		for(var col = 0; col < SIZE; col++){
-			var walls = 0;
-			var keys = Object.keys(OFFSETS);
-			//console.log(keys);
-			for(var key in keys){
-				if(map[row][col][key])walls++;
-			}
-			for(;walls < 3;walls++){
-				if(Math.random() < BORDER_CHANCE){
-					var key = keys[Math.floor(Math.random()*keys.length)];
-					map[row][col][key] = true;
-					var bRow = row + OFFSETS[key].row;
-					var bCol = col + OFFSETS[key].col;
-					if(bRow >= 0 && bRow < SIZE && bCol >= 0 && bCol < SIZE){
-						//console.log("n["+bRow+"]["+bCol+"]["+key+"]");
-						map[bRow][bCol][key] = true;
-					}
-					//console.log("["+row+"]["+col+"]["+key+"]");
+			var p = 0;
+			for(var key in OFFSETS){
+				var nr = row + OFFSETS[key].row;
+				var nc = row + OFFSETS[key].col;
+				if(nr >= 0 && nr < SIZE && nc >= 0 && nc < SIZE){
+					p += 0.05;
 				}
 			}
+			if(Math.random() < p){
+				map[row][col] = 1;
+			}
+
 		}
 	}
-	
-	map.match = function(i, j, k) {
-		if (k=='top') {
-			return map[i][j-1] && map[i][j-1]['bottom']
-		} else if (k=='bottom') {
-			return map[i][j+1] && map[i][j+1]['top']
-		} else if (k=='left') {
-			return map[i-1] && map[i-1][j]['right']
-		} else if (k=='right') {
-			return map[i+1] && map[i+1][j]['left']
-		}
-	};
 
 	return map;
 }
